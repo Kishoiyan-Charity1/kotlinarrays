2e5763612025962a5b25e6baab17e645b0a1fe85
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-03-21 14:22:41 +0300

    first commit

---
+++ src/new.kt
@@ -0,0 +1,12 @@
+fun main() {
+  Introduction("20", "Charity")
+  var name = "mine"
+  println(name.length)
+}
+fun Introduction(age:String,name:String) {
+  val greet= "hi my name is"
+  val age= "and i am "
+  val yrs= "years old"
+  val tog= ("hi my name is " + "charity " + "and i am " + "20 " + "years old")
+  println(tog)
+}
